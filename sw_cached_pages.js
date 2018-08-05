a907e56791a392e50e40c52b88854a37850633c2
William Robertson <williamp.robertson@gmail.com>
2018-08-04 23:07:52 -0700

    file setup through initial caching and clearing unused versions

---
+++ sw_cached_pages.js
@@ -0,0 +1,42 @@
+const cacheName = "v1";
+
+const cacheAssets = [
+  "index.html",
+  "about.html",
+  "contact.html",
+  "assets/style.css",
+  "assets/main.js"
+];
+
+// Call Install Event
+self.addEventListener("install", e => {
+  console.log("Service Worker: Installed");
+
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache => {
+        console.log("Service Worker: Caching Files");
+        cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Call Activate Event
+self.addEventListener("activate", e => {
+  console.log("Service Worker: Activated");
+  // Remove unneeded caches
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== cacheName) {
+            console.log("Service Worker: Clearing Old Cache");
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
