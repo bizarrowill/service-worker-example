de598a4e2040a8c85ac11c82ec1ee1a9f28bcf35
William Robertson <williamp.robertson@gmail.com>
2018-08-04 23:37:24 -0700

    added full site cachine

--- sw_cached_site.js
+++ sw_cached_site.js
@@ -1,32 +1,14 @@
-const cacheName = "v4";
-
-const cacheAssets = [
-  "index.html",
-  "about.html",
-  "contact.html",
-  "assets/style.css",
-  "assets/main.js"
-];
+const cacheName = "v2";
 
 // Call Install Event
 self.addEventListener("install", e => {
   console.log("Service Worker: Installed");
-
-  e.waitUntil(
-    caches
-      .open(cacheName)
-      .then(cache => {
-        console.log("Service Worker: Caching Files");
-        cache.addAll(cacheAssets);
-      })
-      .then(() => self.skipWaiting())
-  );
 });
 
 // Call Activate Event
 self.addEventListener("activate", e => {
   console.log("Service Worker: Activated");
-  // Remove unneeded caches
+  // Clear old caches
   e.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
@@ -44,5 +26,18 @@ self.addEventListener("activate", e => {
 // Call Fetch Event
 self.addEventListener("fetch", e => {
   console.log("Service Worker: Fetching");
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        //Make copy/clone of response
+        const resClone = res.clone();
+        // Open cache
+        caches.open(cacheName).then(cache => {
+          // Add response to cache
+          cache.put(e.request, resClone);
+        });
+        return res;
+      })
+      .catch(err => caches.match(e.request).then(res => res))
+  );
 });
